5b38b6b25a1e4a5b95627a9a95af37208b0d4401
Artyom Slivin <slivin.work@gmail.com>
2022-12-27 18:56:07 +0300

    Parse different types of files.

--- serve_html/main.js
+++ serve_html/main.js
@@ -7,19 +7,57 @@ const getViewUrl = (url) => {
 	return url == '/' ? 'views/index.html' : `views${url}.html`;
 };
 
-http.createServer((req, res) => {
-	let url = getViewUrl(req.url);
-	fs.readFile(url, (error, data) => {
-		if (error) {
-			res.writeHead(httpStatus.StatusCodes.NOT_FOUND);
-			res.write("<h1>FILE NOT FOUND</h1>");
-		} else {
-			res.writeHead(httpStatus.StatusCodes.OK, {
-				"Content-Type": "text/html"
-			});
-			res.write(data);
-		}
-		res.end();
+const sendErrorResponse = res => {
+	res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
+		"Content-Type": "text/html"
 	});
+	res.write("<h1>FILE NOT FOUND</h1>");
+	res.end();
+}
+
+const customReadFile = (file_path, res) => {
+	if (fs.existsSync(file_path)) {
+		fs.readFile(file_path, (error, data) => {
+			if (error) {
+				console.log(error);
+				sendErrorResponse(res);
+				return;
+			}
+			res.write(data);
+			res.end();
+		});
+	} else {
+		sendErrorResponse(res);
+	}
+}
+
+http.createServer((req, res) => {
+	//let url = getViewUrl(req.url);
+	let url = req.url;
+	if (url == "/") {
+		customReadFile(`./views/index.html`, res);
+	} else if (url.indexOf(".html") !== -1) {
+		res.writeHead(httpStatus.StatusCodes.OK, {
+			"Content-Type": "text/html"
+		});
+		customReadFile(`./views${url}`, res);
+	} else if (url.indexOf(".js") !== -1) {
+		res.writeHead(httpStatus.StatusCodes.OK, {
+			"Content-Type": "text/javascript"
+		});
+		customReadFile(`./public/js${url}`, res);
+	} else if (url.indexOf(".css") !== -1) {
+		res.writeHead(httpStatus.StatusCodes.OK, {
+			"Content-Type": "text/css"
+		});
+		customReadFile(`./public/css${url}`, res);
+	} else if (url.indexOf(".png") !== -1) {
+		res.writeHead(httpStatus.StatusCodes.OK, {
+			"Content-Type": "image/png"
+		});
+		customReadFile(`./public/images${url}`, res);
+	} else {
+		sendErrorResponse(res);
+	}
 }).listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
